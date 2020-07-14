ecc82169a26186aa82eb1a47a160e5d4bf495cb4
shinyay <shinya.com@gmail.com>
2020-07-14 23:02:42 +0900

    Add RestTemplate LoadBalanced Annotated

--- src/main/kotlin/com/google/shinyay/SpringCloudLoadbalancerGsApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringCloudLoadbalancerGsApplication.kt
@@ -2,10 +2,19 @@ package com.google.shinyay
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.loadbalancer.LoadBalanced
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
-class SpringCloudLoadbalancerGsApplication
+class SpringCloudLoadbalancerGsApplication {
+
+	@LoadBalanced
+	@Bean
+	fun template() = RestTemplate()
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringCloudLoadbalancerGsApplication>(*args)
 }
+
