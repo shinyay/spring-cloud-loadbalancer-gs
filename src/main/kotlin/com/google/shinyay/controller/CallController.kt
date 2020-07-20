2184748f2d0e23adab9cc1b3e06bc15fb78907d2
shinyay <shinya.com@gmail.com>
2020-07-20 22:11:43 +0900

    Add RestTemplate and Environment by Autowired

--- src/main/kotlin/com/google/shinyay/controller/CallController.kt
+++ src/main/kotlin/com/google/shinyay/controller/CallController.kt
@@ -1,7 +1,10 @@
 package com.google.shinyay.controller
 
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
 
 @RestController
-class CallController {
+class CallController(val template: RestTemplate,
+                     val environment: Environment) {
 }
