f74d0a48d8516ecf4d81234e8085c9429517801c
shinyay <shinya.com@gmail.com>
2020-07-27 22:15:59 +0900

    Add dependency for WebFlux

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ extra["springCloudVersion"] = "Hoxton.SR6"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
