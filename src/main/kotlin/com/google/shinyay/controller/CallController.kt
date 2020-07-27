345562c89678526efbe3829851d71d5590ed456d
shinyay <shinya.com@gmail.com>
2020-07-27 22:48:39 +0900

    Add Spring WebClient as a Load Balancer Client

--- src/main/kotlin/com/google/shinyay/controller/CallController.kt
+++ src/main/kotlin/com/google/shinyay/controller/CallController.kt
@@ -5,9 +5,12 @@ import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 
 @RestController
 class CallController(val template: RestTemplate,
+                     val webClientBuilder: WebClient.Builder,
                      val environment: Environment) {
 
     @GetMapping("/myapp")
@@ -16,5 +19,15 @@ class CallController(val template: RestTemplate,
         val response = template.getForObject(url, String::class.java)
         logger.info("Response: $response")
         return "Port: ${environment.getProperty("local.server.port")} calling -> $response"
+    }
+
+    @GetMapping("/myapp2")
+    fun call2(): Mono<String> {
+        val url = "http://config-client/myapp"
+        logger.info("loadbalance by webclient")
+        return webClientBuilder.build()
+                .get().uri(url)
+                .retrieve()
+                .bodyToMono(String::class.java)
     }
 }
